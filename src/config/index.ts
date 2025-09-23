066ab5564b7bb26ba3fa81b3cf85a37ad410223e
rashedul-dev <rashedulislam.edge@gmail.com>
2025-09-23 13:36:40 +0600

    Add configuration files and environment setup

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+const config = {
+  baseUrl: import.meta.env.VITE_BASE_URL,
+  emailJsServiceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+  emailJsTemplateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+  emailJsPublicKey: import.meta.env.VITE_EMAILJS_PUBLIC_ID,
+};
+
+export default config;
